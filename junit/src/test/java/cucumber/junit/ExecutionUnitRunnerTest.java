55eb1780f0c594464361b2af126177c6f396d40a
Petter Måhlén <pmahlen@shopzilla.com>
2012-06-15 14:50:07 +0200

    removed an unused import statement

--- junit/src/test/java/cucumber/junit/ExecutionUnitRunnerTest.java
+++ junit/src/test/java/cucumber/junit/ExecutionUnitRunnerTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class ExecutionUnitRunnerTest {
     @Test
